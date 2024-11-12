a07afb8b8055c6f8954355537feaf13c39449f5e
Douglas Lowder <douglowder@mac.com>
2024-11-12 02:39:08 -0800

    Implement sleeps till Christmas calculation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Merry Christmas',
+        }}
+      />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { ThemedText as Text } from '@/components/ThemedText';
+import { ThemedView as View } from '@/components/ThemedView';
+import { DateTime, Interval } from 'luxon';
 
 export default function Index() {
+  const date = DateTime.now();
+  const christmas = DateTime.local(2024, 12, 25, 0, 0);
+  const interval = Interval.fromDateTimes(date, christmas);
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{`${date.toLocaleString()}`}</Text>
+      <Text>{`${
+        Math.floor(interval.length('days')) + 1
+      } sleeps until Christmas`}</Text>
     </View>
   );
 }
