9393f97ae528d6093402f878cf323e79ebdf7b54
Douglas Lowder <douglowder@mac.com>
2024-11-25 12:05:26 -0800

    Fix up Nativewind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['app/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    'app/*.{js,jsx,ts,tsx}',
+    'components/*.{js,jsx,ts,tsx}',
+    'constants/*.{js,jsx,ts,tsx}',
+    'hooks/*.{js,jsx,ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
