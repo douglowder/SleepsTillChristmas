13e5ff62b8d52833eebcb7eee72f855dc11066d9
Douglas Lowder <douglowder@mac.com>
2024-12-14 20:49:08 -0800

    Custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
     'hooks/*.{js,jsx,ts,tsx}',
   ],
   presets: [require('nativewind/preset')],
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        throb: 'throb 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        throb: {
+          '0%, 100%': {
+            opacity: '0',
+          },
+          '50%': {
+            opacity: '0.75',
+          },
+        },
+      },
+    },
+  },
   plugins: [],
 };
